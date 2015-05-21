d5bae1b2b400c24d6542002affa98d301160a98b
ozthegreat <ozthegreat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-21 09:41:48 +0000

    Start again
    git-svn-id: https://plugins.svn.wordpress.org/wpml-editor-languages/trunk@1164808 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-wpml-editor-languages-activator.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://ozthegreat.io/wpml-editor-languages
+ * @since      1.0.0
+ *
+ * @package    Wpml_Editor_Languages
+ * @subpackage Wpml_Editor_Languages/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wpml_Editor_Languages
+ * @subpackage Wpml_Editor_Languages/includes
+ * @author     OzTheGreat <edward@ozthegreat.io>
+ */
+class Wpml_Editor_Languages_Activator {
+
+	/**
+	 * Registers any checks to run on plugin activation.
+	 *
+	 * @since   1.0.0
+	 * @return  null
+	 */
+	public static function activate() {
+        self::check_php_version();
+        self::check_reflection_class_exists();
+        self::check_user_can_activate_plugins();
+        self::check_wpml_activated();
+	}
+
+	/**
+	 * Does a check to make sure that the PHP version is equal or
+	 * greater than the WordPress minimum.
+	 *
+	 * @return null
+	 */
+    public static function check_php_version() {
+		global $required_php_version;
+
+		if ( version_compare( PHP_VERSION, $required_php_version ) < 0 )
+        {
+			self::deactivate_plugin( sprintf(
+				wp_kses(
+					__(
+						'WPML Editor Languages requires PHP %s or higher, as does WordPress 3.2 and higher.
+						The plugin has now disabled itself. For more info see the WordPress
+						<a href="%s">requirements page</a>',
+						WPML_EDITOR_LANGUAGES_TEXT_DOMAIN
+					),
+					array(  'a' => array( 'href' => true, 'title' => true, 'target' => true ) )
+				),
+				$required_php_version,
+				esc_url_raw( 'https://wordpress.org/about/requirements/' )
+			) );
+		}
+    }
+
+	/**
+	 * Does a check to make sure that the PHP reflection class
+	 * exists and can be used.
+	 *
+	 * @return null
+	 */
+    public static function check_reflection_class_exists() {
+		if ( ! class_exists("ReflectionClass") )
+        {
+            self::deactivate_plugin( __( 'The PHP ReflectionClass is required to use this plugin. The plugin has now disabled itself.', WPML_EDITOR_LANGUAGES_TEXT_DOMAIN ) );
+		}
+    }
+
+	/**
+	 * Does a check to make sure that the current user has
+	 * the correct permissions to activate plugins.
+	 *
+	 * @return null
+	 */
+    public static function check_user_can_activate_plugins() {
+        if ( ! current_user_can( 'activate_plugins' ) )
+        {
+            self::deactivate_plugin( __( 'You do not have sufficient privileges to activate this plugin.', WPML_EDITOR_LANGUAGES_TEXT_DOMAIN ) );
+		}
+    }
+
+	/**
+	 * Does a check to make sure that WPML is installed and active.
+	 *
+	 * @return null
+	 */
+    public static function check_wpml_activated() {
+        if ( ! is_plugin_active( 'sitepress-multilingual-cms/sitepress.php' ) )
+        {
+			self::deactivate_plugin( sprintf(
+				wp_kses(
+					__(
+						'This plugin is an extension for WPML and is usless without. You can purchase WPML <a href="%s">here</a>',
+						WPML_EDITOR_LANGUAGES_TEXT_DOMAIN
+					),
+					array(  'a' => array( 'href' => true, 'title' => true, 'target' => true ) )
+				),
+				esc_url_raw( 'https://wpml.org/' )
+			) );
+		}
+    }
+
+	/**
+	 * A generic function for deactivating this plugin and
+	 * posting a WordPress die message.
+	 *
+	 * @param  string $error_message Error message to display on die page
+	 * @return null
+	 */
+    public static function deactivate_plugin($error_message) {
+        require_once ABSPATH . '/wp-admin/includes/plugin.php';
+        deactivate_plugins( plugin_basename( __FILE__ ) );
+        wp_die( $error_message );
+    }
+
+}
---
+++ includes/class-wpml-editor-languages.php
@@ -0,0 +1,196 @@
+<?php
+
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://ozthegreat.io/wpml-editor-languages
+ * @since      1.0.0
+ *
+ * @package    Wpml_Editor_Languages
+ * @subpackage Wpml_Editor_Languages/includes
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Wpml_Editor_Languages
+ * @subpackage Wpml_Editor_Languages/includes
+ * @author     Edward D <edward@ozthegreat.io>
+ */
+class Wpml_Editor_Languages {
+
+	/**
+	 * The loader that's responsible for maintaining and registering all hooks that power
+	 * the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Wpml_Editor_Languages_Loader    $loader    Maintains and registers all hooks for the plugin.
+	 */
+	protected $loader;
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct() {
+
+		$this->plugin_name = WPML_EDITOR_LANGUAGES_PLUGIN_NAME;
+		$this->version     = WPML_EDITOR_LANGUAGES_VERSION;
+
+		$this->load_dependencies();
+		$this->set_locale();
+		$this->define_admin_hooks();
+
+	}
+
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - Wpml_Editor_Languages_Loader. Orchestrates the hooks of the plugin.
+	 * - Wpml_Editor_Languages_i18n. Defines internationalization functionality.
+	 * - Wpml_Editor_Languages_Admin. Defines all hooks for the admin area.
+	 * - Wpml_Editor_Languages_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_dependencies() {
+
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpml-editor-languages-loader.php';
+
+		/**
+		 * The class responsible for defining internationalization functionality
+		 * of the plugin.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-wpml-editor-languages-i18n.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the admin area.
+		 */
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-wpml-editor-languages-admin.php';
+
+		$this->loader = new Wpml_Editor_Languages_Loader();
+
+	}
+
+	/**
+	 * Define the locale for this plugin for internationalization.
+	 *
+	 * Uses the Wpml_Editor_Languages_i18n class in order to set the domain and to register the hook
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function set_locale() {
+
+		$plugin_i18n = new Wpml_Editor_Languages_i18n();
+		$plugin_i18n->set_domain( WPML_EDITOR_LANGUAGES_TEXT_DOMAIN );
+
+		$this->loader->add_action( 'plugins_loaded', $plugin_i18n, 'load_plugin_textdomain' );
+
+	}
+
+	/**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_admin_hooks() {
+
+		$plugin_admin = new Wpml_Editor_Languages_Admin( $this->get_plugin_name(), $this->get_version() );
+
+		$this->loader->add_action( 'admin_init', $plugin_admin, 'set_allowed_languages', 10 );
+		$this->loader->add_action( 'login_redirect', $plugin_admin, 'login_allowd_languages_redirect', 10, 3 );
+		$this->loader->add_action( 'edit_user_profile', $plugin_admin, 'add_user_languages_persmissions', 10 );
+		$this->loader->add_action( 'personal_options_update', $plugin_admin, 'save_user_languages_allowed', 10 );
+		$this->loader->add_action( 'edit_user_profile_update', $plugin_admin, 'save_user_languages_allowed', 10 );
+
+	}
+
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run() {
+		$this->loader->run();
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_plugin_name() {
+		return $this->plugin_name;
+	}
+
+	/**
+	 * The reference to the class that orchestrates the hooks with the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    Wpml_Editor_Languages_Loader    Orchestrates the hooks of the plugin.
+	 */
+	public function get_loader() {
+		return $this->loader;
+	}
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version() {
+		return $this->version;
+	}
+
+}
